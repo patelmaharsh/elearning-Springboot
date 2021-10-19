a4a48af4aa5237c7bc793bdd2c2ae2197a0b3c9e
patelmaharsh <maharsh337@gmail.com>
2021-10-19 14:35:13 +0530

    added password encryption for mysql password

--- src/main/java/com/Amdocs/elearning/ELearningApplication.java
+++ src/main/java/com/Amdocs/elearning/ELearningApplication.java
@@ -3,7 +3,10 @@ package com.Amdocs.elearning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ELearningApplication {
 
 	public static void main(String[] args) {
